db79682b97c7cd218a9b5deebde11464a4564fea
Parth_Sharma <parthksharma2811@gmail.com>
2023-01-13 22:54:52 +0530

    second commit

---
+++ adworks-frontend/src/scenes/Services/Composervices.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Compservices() {
+    return ( 
+        
+     );
+}
+
+export default Compservices;
---
+++ adworks-frontend/src/scenes/Services/Services.js
@@ -0,0 +1,12 @@
+import { dividerClasses } from '@mui/material';
+import React from 'react';
+function Services() {
+    return (  
+        <div className='services'>
+            <h3>Our Services</h3>
+            
+        </div>
+    );
+}
+
+export default Service;
