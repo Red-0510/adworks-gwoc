e31ba15f9f99133780ce5fc42ac725e72a6e28f6
Parth_Sharma <parthksharma2811@gmail.com>
2023-01-14 00:06:56 +0530

    3rd

--- adworks-frontend/src/scenes/Services/Composervices.js
+++ adworks-frontend/src/scenes/Services/Composervices.js
@@ -1,7 +1,12 @@
 import React from 'react';
-function Compservices() {
+function Compservices(props) {
     return ( 
-        
+        <div className='compo'>
+        <props.icon/>
+        <div className='texts'><h5 style={{fontSize:props.fontsize}}>{props.title}</h5>
+          <p>{props.text}</p>
+        </div>
+        </div>
      );
 }
 
