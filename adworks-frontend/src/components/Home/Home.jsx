8edeba334d062cc2753a7efc5cda75569ae92d17
Red-0510 <jaineshmachhi@gmail.com>
2023-01-16 22:51:01 +0530

    Revert "Landing Page modified"
    This reverts commit eb0f78162897af1f0c76604402deb2a09f189d47.

---
+++ adworks-frontend/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Carousel from './Carousel'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className='carobody'><Carousel /></div>
+  )
+}
+
+export default Home
--- adworks-frontend/src/components/Navbar/Navbar.jsx
+++ adworks-frontend/src/components/Navbar/Navbar.jsx
@@ -1,99 +1,59 @@
 import React from 'react'
-import { Typography,Button,useTheme,IconButton } from '@mui/material'
+import { Typography,Button,useTheme,IconButton,Avatar } from '@mui/material'
 import {Menu,
   DarkModeOutlined,
   LightModeOutlined,
   SettingsOutlined,
   ImagesearchRoller
+  SettingsOutlined,
+  ImagesearchRoller
 } from "@mui/icons-material";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from 'react-redux';
 // redux
-import { setMode } from "state/store";
+import {setMode} from "state/store"
+import {useDispatch} from "react-redux";
 
 import "./Navbar.css";
 import MyButton from 'components/MyButton/MyButton';
 
-import logo from "assets/images/logo192.png";
-import profile from "assets/images/profile.png";
-import Sidebar from "components/Sidebar/Sidebar";
-import { useNavigate } from "react-router-dom";
-import _ from "lodash";
-const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
-  const theme = useTheme();
-  const user = useSelector((state) => state.global.user);
+import MyButton from 'components/MyButton/MyButton';
 
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+import logo from "assets/images/logo192.png";
+import profile from "assets/images/profile.png"
+const Navbar = ({isSidebarOpen,setIsSidebarOpen}) => {
+  const theme=useTheme();
   const dispatch = useDispatch();
-  const isMobile = useMediaQuery("(max-width:800px)");
-  const navigate = useNavigate();
-  const [active, setActive] = useState(null);
+  const user = useSelector(state=>state.global.user);
   return (
-    <div className="navbar">
-      {isMobile && (
-        <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-          <MenuIcon sx={{ fontSize: "32px" }} />
-        </IconButton>
-      )}
-      <Button
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          m: "0.5rem 0",
-          // gap: "1rem",
-        }}
-      >
+    <div className='navbar'>
+      <IconButton onClick={()=>setIsSidebarOpen(!isSidebarOpen)}>
+        <Menu sx={{fontSize:"32px"}}/>
+      </IconButton>
+      <Button sx={{
+        display:"flex",
+        alignItems:"center",
+        m:"0.5rem 0",
+        gap:"1rem"
+      }}>
         <img src={logo} className="logo" alt="Logo" />
-        <h4>AdWorks</h4>
+        <Typography fontWeight="bold" fontSize="20px" sx={{color:theme.palette.neutral[300]}}>
+          AdWorks
+        </Typography>
       </Button>
-      {!isMobile && (
-        <div className="nav_tabs">
-          {menuList.map(({ text, icon }) => {
-            const textLower = _.kebabCase(text);
-            return (
-                <Button key={text}
-                  onClick={() => {
-                    navigate(`/${textLower}`);
-                    setActive(textLower);
-                  }}
-                  sx={{
-                    backgroundColor: "transparent",
-                    color:"white",
-                    boxShadow:
-                      active === textLower
-                        ? `180px 0 90px -60px inset ${theme.palette.ternary.main} `
-                        : "none",
-                    marginLeft:"10px",
-                  }}
-                >
-                {icon}
-                <Typography
-                    key={text}
-                    sx={{
-                      fontWeight: textLower === active ? "bold" : "400",
-                    }}
-                  >
-                    {text}
-                  </Typography>
-                </Button>
-            );
-          })}
-        </div>
-      )}
-      <div className="info">
-        <IconButton onClick={() => dispatch(setMode())}>
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlined sx={{ fontSize: "32px" }} />
-          ) : (
-            <LightModeOutlined sx={{ fontSize: "32px" }} />
-          )}
+      <div className='info'>
+        <IconButton onClick={()=>dispatch(setMode())}>
+          {
+            (theme.palette.mode==="dark") 
+             ? <DarkModeOutlined sx={{fontSize:"32px"}}/>
+             : <LightModeOutlined sx={{fontSize:"32px"}}/>
+          }
         </IconButton>
         <IconButton>
           <img src={profile} className="logo" />
         </IconButton>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Navbar;
